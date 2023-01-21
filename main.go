053d1ba1e2d6ee26d0ec663dc9865863108fc7dd
Frederick-S <mao_xiaodan@hotmail.com>
2023-01-21 15:05:31 +0800

    add more channels

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 func main() {
 	scheduler := gocron.NewScheduler(time.UTC)
 	_, err := scheduler.Every(1).Day().At("23:59").Do(func() {
-		err := ting.RunTask("https://learningenglish.voanews.com/api/zpyp_e-rm_")
+		channels := []string{"https://learningenglish.voanews.com/api/zpyp_e-rm_", "https://learningenglish.voanews.com/api/ztmp_eibp_", "https://learningenglish.voanews.com/api/zmmpqeb-po", "https://learningenglish.voanews.com/api/zj_pveyrmv", "https://learningenglish.voanews.com/api/zmg_pebmyp", "https://learningenglish.voanews.com/api/zmypyebrmy"}
 
-		if err != nil {
-			log.Printf("Run task error %v\n", err)
+		for _, channel := range channels {
+			err := ting.RunTask(channel)
+
+			if err != nil {
+				log.Printf("Run task error %v\n", err)
+			}
 		}
 	})
 
