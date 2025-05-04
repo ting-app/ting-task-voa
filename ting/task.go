667f756becc17a18707ae0658f4e5bc5a9f6ec00
Frederick-S <mao_xiaodan@hotmail.com>
2025-05-04 11:16:06 +0800

    fix: ensure published date matches start date

--- ting/task.go
+++ ting/task.go
@@ -27,7 +27,7 @@ func RunTask(channel Channel) error {
 	var voas []*Voa
 
 	for _, item := range feed.Items {
-		if strings.HasSuffix(item.Link, ".html") && startDate.Before(*item.PublishedParsed) {
+		if strings.HasSuffix(item.Link, ".html") && isSameDay(startDate, *item.PublishedParsed) {
 			voa, err := parseVoa(item.Link)
 
 			if err != nil {
@@ -94,4 +94,11 @@ func RunTask(channel Channel) error {
 	log.Printf("Saved %v news as ting, url=%s\n", savedTing, url)
 
 	return nil
+}
+
+func isSameDay(t1, t2 time.Time) bool {
+	y1, m1, d1 := t1.Date()
+	y2, m2, d2 := t2.Date()
+
+	return y1 == y2 && m1 == m2 && d1 == d2
 }
